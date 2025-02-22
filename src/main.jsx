93bc232ca16a562fdabc17cb3acd3cade931feb0
Santanu Raj <santanu.raj.30.01@gmail.com>
2025-02-22 17:52:48 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import HomePage from './components/homepage.jsx'
 import CreateCampaign from './components/CreateCampaign.jsx'
 import CampaignCard from './components/CampaignCard.jsx'
 import CampaignPage from './components/CampaignPage.jsx'
+import ContactUs from './components/ContactUs.jsx'
+import AboutUS from './components/AboutUS.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -18,5 +20,7 @@ createRoot(document.getElementById('root')).render(
     <CampaignPage />
     <footer/>
     <button/>
+    <ContactUs />
+    <AboutUS/>
   </StrictMode>,
 )
