9d9e63530e0261f68cbaede0a23d87db37954718
Santanu Raj <santanu.raj.30.01@gmail.com>
2025-03-06 14:13:37 +0530

    new

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,7 +2,7 @@ function ContactUs() {
   return (
     <div className="contact-us">
       <h2>Contact Us</h2>
-      <p>Email: support@crowdfund.com</p>
+      <p>Email: santanu.raj.30.01@gmai.com</p>
       <img src="https://www.cloudways.com/blog/wp-content/uploads/CrowdFunding-platforms.jpg" alt="Contact Us" className="contact-image" />
     </div>
   );
